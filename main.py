58b88f2b5510b2f62c83710b43433c6ae30d9996
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-21 18:32:25 +0200

    feat: add support to call main with position args

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import sys
+
 from vacancies_analysis import analysis, visualization
 from vacancies_scraper import scraper
 
 
 if __name__ == "__main__":
-    scraper.scrape_vacancies()
+    if len(sys.argv) < 2:
+        print("\nUsage: python main.py <vacancy input>")
+        exit(1)
+
+    transformed_position = ("+".join(sys.argv[1:])).lower() + "/"
+
+    scraper.scrape_vacancies(transformed_position)
     analysis.analyze_technologies()
     visualization.plot_top_technologies()
