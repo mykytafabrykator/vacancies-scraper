c0a9c07b98d8cf437e96fe6545641fbb0c141b47
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-21 18:03:36 +0200

    feat: synchronize call of visualization after getting stats

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from vacancies_analysis import analysis
+from vacancies_analysis import analysis, visualization
 from vacancies_scraper import scraper
 
 
 if __name__ == "__main__":
     scraper.scrape_vacancies()
     analysis.analyze_technologies()
+    visualization.plot_top_technologies()
